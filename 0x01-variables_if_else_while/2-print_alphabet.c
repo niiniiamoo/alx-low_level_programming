12c559efbfcccfce99fb5f1d61dc828060cdfbd7
Titus <titusdecardinelson@gmail.com>
2023-02-19 19:06:46 -0800

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+  * main - entry point
+  * Return: always 0
+*/
+int main(void)
+{	char alpha;
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		putchar(alpha);
+	putchar('\n');
+	return (0);
+}
