60985308232219672a2dfbbc614e11dbde887be0
Titus <titusdecardinelson@gmail.com>
2023-02-24 17:34:56 -0800

    omit 2 n 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  * print_most_numbers - print 0 - 9 except 2 and 4
+  * Return: always 0
+*/
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+	if (c == 50 || c == 52)
+		continue;
+	putchar(c);
+	}
+	putchar('\n');
+}
