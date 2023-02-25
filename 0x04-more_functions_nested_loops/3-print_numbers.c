075470c6d1df2176182f753cae2a3706225e652e
Titus <titusdecardinelson@gmail.com>
2023-02-24 17:03:44 -0800

    prints the integers 0 - 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  * print_numbers - prints the integers 0 - 9
+  *
+  * Return: Always 0 (Success)
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		_putchar(i);
+		_putchar('\n');
+}
